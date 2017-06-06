e773a67b2947cdbbb7fee2879818139e909d7a75
Vyshakh Kandamath <kvyshakh@terpmail.umd.edu>
2017-06-05 21:15:42 -0400

    modified projects

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,25 @@ function changeNavbar() {
 		$(".navbar-default").css("border-color", "rgba(255,255,255,.3)");
 		$(".navbar-default").css("border-bottom-width", "1px");
 		$(".navbar-brand").css("color", "white");
-		$(".navbar-nav > li > a").css("color", "White");
+		$(".navbar-nav > li > a").css("color", "white");
+		$(".navbar-nav > li > a").hover(function(){
+		    $(this).css("color", "#2E9CCA");
+		    }, function(){
+		    $(this).css("color", "white");
+		});
 		$("#last-name").css("color", "white")
 	}
 	else {
 		$(".navbar-default").css("background-color", "white");
 		$(".navbar-default").css("border-color", "#DCDCDC");
 		$(".navbar-default").css("border-bottom-width", "2px");
-		$(".navbar-brand").css("color", "#2F2D2E");
-		$(".navbar-nav > li > a").css("color", "#2F2D2E");
+		$(".navbar-brand").css("color", "#252525");
+		$(".navbar-nav > li > a").css("color", "#252525");
+		$(".navbar-nav > li > a").hover(function(){
+		    $(this).css("color", "#2E9CCA");
+		    }, function(){
+		    $(this).css("color", "#252525");
+		});
 		$("#last-name").css("color", "#2E9CCA")
 	}
 }
@@ -24,4 +34,15 @@ $(function(){
 			strings: ["build things.", "craft experiences."],
 			typeSpeed: 0
 		});
-	})
+
+		$("#collapsed-navbar").on("show.bs.collapse", function() {
+			$("#collapsed-navbar").removeClass("pull-right");
+			$("#collapsed-navbar").css("background-color", "#252525");
+			$("#collapsed-navbar").css("border-top", "none");
+		});
+
+		$("#collapsed-navbar").on("hidden.bs.collapse", function() {
+			$("#collapsed-navbar").addClass("pull-right");
+			$("#collapsed-navbar").css("background-color", "inherit");
+		});
+});
