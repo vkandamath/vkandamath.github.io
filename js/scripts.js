4fc37618dc55a909205450fbe17d67f3af48abec
Vyshakh Kandamath <kvyshakh@terpmail.umd.edu>
2017-09-22 19:03:28 -0400

    navbar transition bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 window.onscroll = function() {changeNavbar()};
 
 function changeNavbar() {
-	if (document.body.scrollTop <= 50) {
+	var bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	if (bodyScrollTop <= 50) {
 		$(".navbar-default").css("background-color", "transparent");
 		$(".navbar-default").css("border-color", "rgba(255,255,255,.3)");
 		$(".navbar-default").css("border-bottom-width", "1px");
